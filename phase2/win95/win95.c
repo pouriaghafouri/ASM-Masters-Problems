c29bd78d1277a14637ade853c0cf63f3919531e7
Hirbod Behnam <hirbod.behnam.81@gmail.com>
2024-12-31 18:04:03 +0330

    Added win95

---
+++ phase2/win95/win95.c
@@ -0,0 +1,107 @@
+// From https://infosecwriteups.com/reversing-microsofts-windows95-product-key-check-mechanism-ca7e825014b6
+#include <memory.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+
+bool mod7_check(const char *param_1)
+{
+    int IndexCounter;
+    int SUM;
+    char character;
+
+    IndexCounter = 0;
+    SUM = 0;
+    while (true)
+    {
+        character = *param_1;
+        if ((character < '0') || ('9' < character))
+            break;
+        /* Convert Char to Int ?? */
+        SUM = SUM + character + -48;
+        IndexCounter = IndexCounter + 1;
+        param_1 = param_1 + 1;
+        if (6 < IndexCounter)
+        {
+            return SUM % 7 == 0;
+        }
+    }
+    return false;
+}
+
+int check_oem_key(const char *key, int len)
+
+{
+    int key_length;
+    int is_oem;
+    int first_3_digits_int;
+    int second_2_digits_int;
+    int i;
+    int i_;
+    char first_three_digits[4];
+    char second_two_digits[3];
+    char kc;
+
+    key_length = strlen(key);
+    if ((((key_length == 0x17) && (key[5] == '-')) && (key[9] == '-')) &&
+        ((key[0x11] == '-' && (is_oem = memcmp(key + 6, "OEM", 3), is_oem == 0))))
+    {
+        /* Check first 5 digits, must be numeric */
+        i = 0;
+        do
+        {
+            kc = key[i];
+            if (kc < '0')
+            {
+                return 0;
+            }
+            if ('9' < kc)
+            {
+                return 0;
+            }
+            i = i + 1;
+        } while (i < 5);
+        /*  */
+        memcpy(first_three_digits, key, MIN(3, len));
+        first_three_digits[3] = '\0';
+        first_3_digits_int = atoi(first_three_digits);
+        if ((first_3_digits_int != 0) &&
+            (first_3_digits_int < 367
+             /* check if between 1 and 366 */))
+        {
+            memcpy(second_two_digits, key + 3, MIN(2, len));
+            second_two_digits[2] = '\0';
+            second_2_digits_int = atoi(second_two_digits);
+            if (((second_2_digits_int < 3) || (94 < second_2_digits_int)) &&
+                (((key[10] == '0' && ('0' < key[0x10])) && (key[0x10] < '8'))))
+            {
+                i_ = 0x12;
+                while ((kc = key[i_], '/' < kc && (kc < ':')))
+                {
+                    i_ = i_ + 1;
+                    if (0x16 < i_)
+                    {
+                        /* jump to part after oem- */
+                        return mod7_check(key + 10);
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    char serial[128];
+    printf("Enter the serial number: ");
+    fgets(serial, sizeof(serial), stdin);
+    serial[strlen(serial) - 1] = 0;
+    if (check_oem_key(serial, sizeof(serial)))
+        puts("Product activated!");
+    else
+        puts("Invalid activation code!");
+    return 0;
+}
